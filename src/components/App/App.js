89a8005216bd10f956286b608aa8434dbb04c7a5
Fialka Lesnaya <listik.vs@gmail.com>
2023-07-15 23:35:56 +0300

    React router Добавлен

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,43 @@
-import Main from "../Main/Main";
+import { Route, Routes } from 'react-router-dom';
+import Main from '../Main/Main';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Main/>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        
+        <Route
+          path='/movies'
+          element={<div>movies</div>}
+        />
+
+        <Route
+          path='/saved-movies'
+          element={<div>saved-movies</div>}
+        />
+
+        <Route
+          path='/profile'
+          element={<div>Login</div>}
+        />
+
+        <Route
+          path='/signin'
+          element={<div>sign-in</div>}
+        />
+
+        <Route
+          path='/signup'
+          element={<div>sign-in</div>}
+        />
+
+        <Route
+          path='*'
+          element={<div>404</div>}
+        />
+      </Routes>
     </div>
   );
 }
