98b71d9ea9eac5c4861cc48f9d8c5daf57f36c20
Fialka Lesnaya <listik.vs@gmail.com>
2023-07-17 00:03:23 +0300

    FilterCheckbox компонент

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,14 @@
 import Footer from 'components/common/Footer/Footer';
 import './Movies.css';
 import Header from 'components/common/Header/Header';
+import FilterCheckbox from 'components/common/FilterCheckbox/FilterCheckbox';
 
 function Movies() {
   return (
     <div>
       <Header isMainPage={false} />
       Movies
+      <FilterCheckbox />
       <Footer />
     </div>
   );
--- src/components/common/Input/Input.js
+++ src/components/common/Input/Input.js
@@ -1,17 +1,17 @@
 import './Input.css';
 
-function Input({ isError = false, type = 'text' }) {
+function Input({ isError = false, type = 'text', uniqId='input-name' }) {
   return (
     <div className={'input' + (isError ? ' input--error' : '')}>
-      <label htmlFor='input-name' className='input__label'>
+      <label htmlFor={uniqId} className='input__label'>
         Login
       </label>
 
       <input
         type={type}
         className='input__element'
-        id='input-name'
-        name='input-name'
+        id={uniqId}
+        name={uniqId}
       />
 
       {isError && <p className='input__error'>Что-то пошло не так...</p>}
