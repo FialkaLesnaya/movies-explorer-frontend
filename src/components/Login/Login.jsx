38e5e91073ce1900eeb7a866a3d407368001685a
Fialka Lesnaya <listik.vs@gmail.com>
2023-07-19 00:04:01 +0300

    Блок Login и адаптивная верстка

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,29 @@
 import Input from 'components/common/Input/Input';
 import './Login.css';
+import Logo from 'components/common/Logo/Logo';
 
 function Login() {
   return (
-    <div>
-    Login
+    <div className='login'>
+      <Logo />
 
-    <Input />
-    <Input isError />
+      <h2 className='login__title'>Рады видеть!</h2>
+
+      <form className='login__form'>
+        <div className='login__input'>
+          <Input text='Email' type='email' uniqId='email' />
+        </div>
+
+        <div className='login__input'>
+          <Input text='Пароль' type='password' uniqId='password' />
+        </div>
+
+        <button className='login__submit' type='submit'>Войти</button>
+
+        <p className='login__info'>
+          Ещё не зарегистрированы? <a className='login__link' href='/signup'>Регистрация</a>
+        </p>
+      </form>
     </div>
   );
 }
