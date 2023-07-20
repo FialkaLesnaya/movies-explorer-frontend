b63b5280e2f8d0990aee8c1f34a3c6bef9e89a3f
Fialka Lesnaya <listik.vs@gmail.com>
2023-07-21 01:12:10 +0300

    Блок SavedMovies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -5,12 +5,99 @@ import FilterCheckbox from 'components/common/FilterCheckbox/FilterCheckbox';
 import MoviesCardList from './MoviesCardList/MoviesCardList';
 
 function Movies() {
+  const cardList = [
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/583809524/photo/alberta-wilderness-near-banff.jpg?s=2048x2048&w=is&k=20&c=W2WIEh64AZfOEd7TuMzWQUVIBWXiHpytdWHDNEKxhPo=',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: true,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: true,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: true,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image:
+        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
+      isLiked: false,
+    },
+  ];
+
   return (
     <div>
       <Header isMainPage={false} />
       <div className='movies__container'>
         <FilterCheckbox />
-        <MoviesCardList />
+        <MoviesCardList cardList={cardList} />
       </div>
       <Footer />
     </div>
--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,85 +1,11 @@
 import MoviesCard from '../MoviesCard/MoviesCard';
 import './MoviesCardList.css';
 
-function MoviesCardList() {
-  const cardList = [
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/583809524/photo/alberta-wilderness-near-banff.jpg?s=2048x2048&w=is&k=20&c=W2WIEh64AZfOEd7TuMzWQUVIBWXiHpytdWHDNEKxhPo=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: true,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: true,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: true,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image: 'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: false,
-    },
-  ];
+function MoviesCardList({ cardList, isFavoriteBlock = false }) {
   return (
     <div className='movies-card-list'>
       {cardList.map((item) => (
-        <MoviesCard item={item} />
+        <MoviesCard item={item} isFavoriteBlock={isFavoriteBlock} />
       ))}
     </div>
   );
