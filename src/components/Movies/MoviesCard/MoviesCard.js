14f0dfbeb3bb6c3605163fbd17c1b1d01fae34ff
Fialka Lesnaya <listik.vs@gmail.com>
2023-07-21 00:56:31 +0300

    Анимация добавлена

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,16 +1,17 @@
 import './MoviesCard.css';
 
-function MoviesCard() {
+function MoviesCard({ item: { title, duration, isLiked } }) {
+  const iconStyle = isLiked ? 'movies-card__action--fill' : 'movies-card__action--not-fill';
   return (
     <div className='movies-card'>
       <div className='movies-card__header'>
         <div className='movies-card__info'>
-          <h3 className='movies-card__title'>33 слова о дизайне</h3>
+          <h3 className='movies-card__title'>{title}</h3>
 
-          <p className='movies-card__time'>1ч 47м</p>
+          <p className='movies-card__time'>{duration}</p>
         </div>
 
-        <button className='movies-card__action movies-card__action--not-fill' type='button'></button>
+        <button className={'movies-card__action ' + iconStyle} type='button'></button>
       </div>
 
       <div className='movies-card__photo'></div>
--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -2,14 +2,85 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 import './MoviesCardList.css';
 
 function MoviesCardList() {
+  const cardList = [
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: true,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: true,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: true,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: false,
+    },
+    {
+      title: '33 слова о дизайне',
+      duration: '1ч 47м',
+      image: '',
+      isLiked: false,
+    },
+  ];
   return (
     <div className='movies-card-list'>
-      <MoviesCard />
-      <MoviesCard />
-      <MoviesCard />
-
-      <MoviesCard />
-      <MoviesCard />
+      {cardList.map((item) => (
+        <MoviesCard item={item} />
+      ))}
     </div>
   );
 }
