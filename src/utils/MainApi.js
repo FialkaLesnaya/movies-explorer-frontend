b3747adbe3b1cdbf2a982da659e728cb04cd757f
Fialka Lesnaya <listik.vs@gmail.com>
2023-07-26 23:19:14 +0300

    Файлы Api

---
+++ src/utils/MainApi.js
@@ -0,0 +1,91 @@
+class MainApiService {
+  constructor(options) {
+    this.baseUrl = options.baseUrl;
+    this.headers = options.headers;
+  }
+
+  _request(url, options) {
+    return fetch(url, options).then(this._checkResponse);
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  register(name, password, email) {
+    return this._request(`${this.baseUrl}/signup`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: name,
+        password: password,
+        email: email,
+      }),
+    });
+  }
+
+  login(password, email) {
+    return this._request(`${this.baseUrl}/signin`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        password: password,
+        email: email,
+      }),
+    });
+  }
+
+  editProfile(name, email) {
+    return this._request(`${this.baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    });
+  }
+  
+  checkMe(jwt) {
+    return this._request(`${this.baseUrl}/users/me`, {
+      method: "GET",
+      headers: {
+        ...this.headers,
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+  }
+
+  loadSavedMovies() {
+    return this._request(`${this.baseUrl}/movies`, { headers: this.headers });
+  }
+
+  likeMovie(data) {
+    return this._request(`${this.baseUrl}/movies`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        data: data,
+      }),
+    });
+  }
+
+  deleteLikeMovie(movieId) {
+    return this._request(`${this.baseUrl}/movies/${movieId}`, {
+      method: 'DELETE',
+      headers: this.headers,
+    });
+  }
+}
+
+export const MainApi = new MainApiService({
+  baseUrl: 'rocks',
+  headers: {
+    authorization: localStorage.getItem('JWT_SECRET_KEY'),
+    'Content-Type': 'application/json',
+  },
+});
