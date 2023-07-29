e8516f426d3036e19f8cedce54c63f4a968d38d4
Fialka Lesnaya <listik.vs@gmail.com>
2023-07-30 00:51:04 +0300

    Удаление

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -4,47 +4,50 @@ import Header from 'components/common/Header/Header';
 import LoadingButton from 'components/common/LoadingButton/LoadingButton';
 import SearchForm from 'components/common/SearchForm/SearchForm';
 import MoviesCardList from 'components/common/MoviesCardList/MoviesCardList';
-import useLoadSavedMovies from 'hooks/useLoadSavedMovies';
 import useCardLimit from 'hooks/useCardLimit';
 import useSearch from 'hooks/useSearch';
 import MoviesCardListStates from 'components/common/MoviesCardListStates/MoviesCardListStates';
 
 function SavedMovies() {
-  const { initialSavedMovies, isError, isLoading } = useLoadSavedMovies();
-  const { movies, handleSearch, hasSearchValue } = useSearch({ initialMovies: initialSavedMovies });
-  const { cardLimit, handleLoadMore } = useCardLimit();
-
   return (
-    <>
-      <Header />
-      <main className='saved-movies'>
-        <section className='saved-movies__container'>
-          <SearchForm handleSearch={handleSearch} />
+    <div>
+    then</div>
+  )
+  // const { initialSavedMovies, isError, isLoading } = useLoadSavedMovies();
+  // const { movies, handleSearch, hasSearchValue } = useSearch({ initialMovies: initialSavedMovies });
+  // const { cardLimit, handleLoadMore } = useCardLimit();
 
-          <MoviesCardListStates
-            isError={isError}
-            isLoading={isLoading}
-            nothingFound={movies.length === 0 && hasSearchValue}
-            tryToSearch={movies.length === 0 && !hasSearchValue}
-          />
+  // return (
+  //   <>
+  //     <Header />
+  //     <main className='saved-movies'>
+  //       <section className='saved-movies__container'>
+  //         <SearchForm handleSearch={handleSearch} />
 
-          {movies.length > 0 && (
-            <MoviesCardList
-              cardList={initialSavedMovies}
-              limit={cardLimit}
-              isFavoriteBlock
-            />
-          )}
+  //         <MoviesCardListStates
+  //           isError={isError}
+  //           isLoading={isLoading}
+  //           nothingFound={movies.length === 0 && hasSearchValue}
+  //           tryToSearch={movies.length === 0 && !hasSearchValue}
+  //         />
 
-          {movies.length > 0 &&
-            movies.length > cardLimit && (
-              <LoadingButton onChange={handleLoadMore} />
-            )}
-        </section>
-      </main>
-      <Footer />
-    </>
-  );
+  //         {movies.length > 0 && (
+  //           <MoviesCardList
+  //             cardList={initialSavedMovies}
+  //             limit={cardLimit}
+  //             isFavoriteBlock
+  //           />
+  //         )}
+
+  //         {movies.length > 0 &&
+  //           movies.length > cardLimit && (
+  //             <LoadingButton onChange={handleLoadMore} />
+  //           )}
+  //       </section>
+  //     </main>
+  //     <Footer />
+  //   </>
+  // );
 }
 
 export default SavedMovies;
