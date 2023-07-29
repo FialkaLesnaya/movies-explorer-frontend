7709a6a5fe95d64e73d0b4e3a7433406f346f060
Fialka Lesnaya <listik.vs@gmail.com>
2023-07-29 22:14:50 +0300

    Блок сохраненных фильмов

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -4,40 +4,42 @@ import Header from 'components/common/Header/Header';
 import LoadingButton from 'components/common/LoadingButton/LoadingButton';
 import SearchForm from 'components/common/SearchForm/SearchForm';
 import MoviesCardList from 'components/common/MoviesCardList/MoviesCardList';
+import useLoadSavedMovies from 'hooks/useLoadSavedMovies';
+import useCardLimit from 'hooks/useCardLimit';
+import useSearch from 'hooks/useSearch';
+import MoviesCardListStates from 'components/common/MoviesCardListStates/MoviesCardListStates';
 
 function SavedMovies() {
-  const cardList = [
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image:
-        'https://media.istockphoto.com/id/583809524/photo/alberta-wilderness-near-banff.jpg?s=2048x2048&w=is&k=20&c=W2WIEh64AZfOEd7TuMzWQUVIBWXiHpytdWHDNEKxhPo=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image:
-        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: false,
-    },
-    {
-      title: '33 слова о дизайне',
-      duration: '1ч 47м',
-      image:
-        'https://media.istockphoto.com/id/1092924028/photo/colorful-painted-hands-in-front-of-a-spring-scene.jpg?s=2048x2048&w=is&k=20&c=2o2PSOt_Y88mFMuWDgU0qaH6knB7IFZeYZdPbBqCfN4=',
-      isLiked: true,
-    },
-  ];
+  const { initialSavedMovies, isError, isLoading } = useLoadSavedMovies();
+  const { movies, handleSearch, hasSearchValue } = useSearch({ initialMovies: initialSavedMovies });
+  const { cardLimit, handleLoadMore } = useCardLimit();
 
   return (
     <>
       <Header />
       <main className='saved-movies'>
         <section className='saved-movies__container'>
-          <SearchForm />
-          <MoviesCardList cardList={cardList} isFavoriteBlock />
-          <LoadingButton />
+          <SearchForm handleSearch={handleSearch} />
+
+          <MoviesCardListStates
+            isError={isError}
+            isLoading={isLoading}
+            nothingFound={movies.length === 0 && hasSearchValue}
+            tryToSearch={movies.length === 0 && !hasSearchValue}
+          />
+
+          {movies.length > 0 && (
+            <MoviesCardList
+              cardList={initialSavedMovies}
+              limit={cardLimit}
+              isFavoriteBlock
+            />
+          )}
+
+          {movies.length > 0 &&
+            movies.length > cardLimit && (
+              <LoadingButton onChange={handleLoadMore} />
+            )}
         </section>
       </main>
       <Footer />
---
+++ src/hooks/useLoadSavedMovies.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import { MainApi } from 'utils/MainApi';
+
+const useLoadSavedMovies = () => {
+  const [initialSavedMovies, setInitialSavedMovies] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    MainApi.loadSavedMovies()
+      .then((data) => {
+        setInitialSavedMovies(data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsError(true);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return {
+    initialSavedMovies,
+    isError,
+    isLoading,
+  };
+};
+
+export default useLoadSavedMovies;
