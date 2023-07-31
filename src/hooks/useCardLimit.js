55960acc1d55043d8253a02c8e18e4212f39593f
Fialka Lesnaya <listik.vs@gmail.com>
2023-08-01 00:49:02 +0300

    Лимит карточек установлен

--- src/hooks/useCardLimit.js
+++ src/hooks/useCardLimit.js
@@ -1,11 +1,48 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
+
+const DESKTOP_ROW_ITEMS = 3;
+const TABLET_ROW_ITEMS = 2;
+const MOBILE_ROW_ITEMS = 1;
+const DESKTOP_COLUMN_ITEMS = 4;
+const TABLET_COLUMN_ITEMS = 4;
+const MOBILE_COLUMN_ITEMS = 5;
 
 const useCardLimit = () => {
-  const [cardLimit, setCardLimit] = useState(3 * 4);
+  const [cardLimit, setCardLimit] = useState(
+    DESKTOP_ROW_ITEMS * DESKTOP_COLUMN_ITEMS
+  );
+
+  const updateCardLimit = useCallback(() => {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 1280) {
+      setCardLimit(DESKTOP_ROW_ITEMS * DESKTOP_COLUMN_ITEMS);
+    } else if (screenWidth >= 768) {
+      setCardLimit(TABLET_ROW_ITEMS * TABLET_COLUMN_ITEMS);
+    } else {
+      setCardLimit(MOBILE_ROW_ITEMS * MOBILE_COLUMN_ITEMS);
+    }
+  }, []);
 
   const handleLoadMore = useCallback(() => {
-    setCardLimit(cardLimit + 3);
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 1280) {
+      setCardLimit(cardLimit + DESKTOP_ROW_ITEMS);
+    } else if (screenWidth >= 768) {
+      setCardLimit(cardLimit + TABLET_ROW_ITEMS);
+    } else {
+      setCardLimit(cardLimit + 2);
+    }
   }, [cardLimit]);
+
+  useEffect(() => {
+    updateCardLimit();
+    window.addEventListener('resize', updateCardLimit);
+    return () => {
+      window.removeEventListener('resize', updateCardLimit);
+    };
+  }, [updateCardLimit]);
 
   return {
     cardLimit,
